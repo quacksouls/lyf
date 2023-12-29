4b29a8a39d65f7f4dcd285c7ebea1ad269f0ba3a
quacksouls <66396308+quacksouls@users.noreply.github.com>
2023-12-29 19:22:29 +1100

    Remove junk files, remnants from honkit

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Run this script to cleanse the Markdown files and convert them to
-# HonKit book.
-
-npm run clean
-npx honkit build
-cp -R _book/* .
--- view.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Run this script to build the book and view your changes locally.
-
-npm run clean
-npx honkit serve
